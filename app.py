774f4db6070c5a1b71b3218cd746c28685495af6
yuta <katayama@xshell.io>
2018-10-26 14:23:08 +0900

    Add ability to collect cpu usage

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import ambient
 import time
 import os
 import sys
+import psutil
 
 try:        
     AMBIENT_CHANNEL_ID = int(os.environ['AMBIENT_CHANNEL_ID'])
@@ -14,11 +15,15 @@ am = ambient.Ambient(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY)
 
 def get_temp():
     return int(open('/sys/class/thermal/thermal_zone0/temp').read())/1000.0
+    
+def get_cpu_percent():
+    return psutil.cpu_percent()
 
 def request():
     am.send({
         'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'd1': get_temp(),
+        'd2': get_cpu_percent()
     })
 
 if __name__ == '__main__':
